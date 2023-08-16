170eb7d2cc04e186824d7c1d1b9391f86fb2f9e8
MahRukh <mahrukhdev@gmail.com>
2023-08-16 15:01:40 +0500

    SignIn and SignUp fragments added

---
+++ app/src/main/java/com/mahrukhdev/petfinderapp_kotlin/ui/views/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mahrukhdev.petfinderapp_kotlin.ui.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahrukhdev.petfinderapp_kotlin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
