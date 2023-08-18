4291641e536e83e524bf6e0cfac4e94cb24321d8
MahRukh <mahrukhdev@gmail.com>
2023-08-18 11:48:40 +0500

    UI of sign in and sign up fragments implemented

--- app/src/main/java/com/mahrukhdev/petfinderapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/mahrukhdev/petfinderapp_kotlin/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.mahrukhdev.petfinderapp_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.mahrukhdev.petfinderapp_kotlin.ui.views.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_main)
+
+        // on below line we are calling
+        // handler to run a task
+        // for specific time interval
+        Handler().postDelayed({
+            // on below line we are
+            // creating a new intent
+            val i = Intent(
+                this@MainActivity,
+                LoginActivity::class.java
+            )
+            // on below line we are
+            // starting a new activity.
+            startActivity(i)
+
+            // on the below line we are finishing
+            // our current activity.
+            finish()
+        }, 2000)
+
     }
 }
